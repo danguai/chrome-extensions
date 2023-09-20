6c87e6676da3cfc1d69992986731999780cc7c71
Daniel Blanco <db@d-blanco.com>
2023-09-20 17:36:09 -0400

    React-App Fixed

--- extension/scripts/changeBgColor.js
+++
@@ -1,5 +0,0 @@
-/*global chrome*/
-
-chrome.storage.sync.get('color', ({ color }) => {
-    document.body.style.backgroundColor = color;
-});
--- react-chrome-app/src/index.js
+++ react-chrome-app/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.createElement('div');
 rootElement.id = 'react-chrome-app';
-document.body.appendChild(rootElement);
 
 const globalStyles = document.createElement('style');
 globalStyles.innerHTML = `
-#${rootElement.id} {
+  #${rootElement.id} {
   position: fixed;
   left: 0;
   top: 0;
@@ -19,38 +16,42 @@ globalStyles.innerHTML = `
   background: #ffffff;
   border-right: 1px solid #c2c2c2;
   z-index: 999
-}`;
+  }
+`;
+
 rootElement.appendChild(globalStyles);
 document.body.appendChild(rootElement);
 
-const Button = styled.a < { $primary?: boolean } > `
-  --accent-color: white;
+// const Button = styled.a < { $primary?: boolean } > `
+//   --accent-color: white;
 
-  background: transparent;
-  border-radius: 3px;
-  border: 1px solid var(--accent-color);
-  color: var(--accent-color);
-  display: inline-block;
-  margin: 0.5rem 1rem;
-  padding: 0.5rem 0;
-  transition: all 200ms ease-in-out;
-  width: 11rem;
+//   background: transparent;
+//   border-radius: 3px;
+//   border: 1px solid var(--accent-color);
+//   color: var(--accent-color);
+//   display: inline-block;
+//   margin: 0.5rem 1rem;
+//   padding: 0.5rem 0;
+//   transition: all 200ms ease-in-out;
+//   width: 11rem;
 
-  &:hover {
-    filter: brightness(0.85);
-  }
+//   &:hover {
+//     filter: brightness(0.85);
+//   }
 
-  &:active {
-    filter: brightness(1);
-  }
+//   &:active {
+//     filter: brightness(1);
+//   }
 
-  ${props => props.$primary && css`
-    background: var(--accent-color);
-    color: black;
-  `}
-`
+//   ${props => props.$primary && css`
+//     background: var(--accent-color);
+//     color: black;
+//   `}
+// `
+// rootElement.appendChild(Button);
+// document.body.appendChild(rootElement);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
